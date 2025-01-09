9192357a44eb624a7184c81c8865d145cf90a0b2
Alexandre Lacroix <alacroix@bess-f4r4s12.clusters.42paris.fr>
2025-01-09 18:48:04 +0100

    quick fix for len of 6 stack

--- src/sorting/brut_force.c
+++ src/sorting/brut_force.c
@@ -6,38 +6,11 @@
 /*   By: alacroix <alacroix@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 12:10:40 by alacroix          #+#    #+#             */
-/*   Updated: 2025/01/07 12:55:15 by alacroix         ###   ########.fr       */
+/*   Updated: 2025/01/09 18:47:07 by alacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-
-static void	push_min(t_node **src_head, t_node **dst_head, int size, char stack)
-{
-	t_node	*v_min;
-	char	other;
-	int		i;
-
-	if (stack == 'a')
-		other = 'b';
-	else
-		other = 'a';
-	i = 0;
-	v_min = NULL;
-	while ((size - i) > 3)
-	{
-		v_min = search_vmin_node(src_head);
-		put_vmin_at_head(src_head, &v_min);
-		push(src_head, dst_head, other);
-		i++;
-	}
-	brut_force(src_head, dst_head, size, stack);
-	while (i)
-	{
-		push(dst_head, src_head, stack);
-		i--;
-	}
-}
 
 static void	size_of_two(t_node **a_head, t_node *next, char stack)
 {
@@ -74,6 +47,34 @@ static void	size_of_three(t_node **a, t_node *b, t_node *c, char stack)
 		reverse_rotate(a, stack);
 	else
 		return ;
+}
+
+static void	push_min(t_node **src_head, t_node **dst_head, int size, char stack)
+{
+	t_node	*v_min;
+	char	other;
+	int		i;
+
+	if (stack == 'a')
+		other = 'b';
+	else
+		other = 'a';
+	i = 0;
+	v_min = NULL;
+	while ((size - i) > 3)
+	{
+		v_min = search_vmin_node(src_head);
+		put_vmin_at_head(src_head, &v_min);
+		push(src_head, dst_head, other);
+		i++;
+	}
+	size_of_three(src_head, ((t_node *)(*dst_head))->next,
+		((t_node *)(*dst_head))->prev, stack);
+	while (i > 0)
+	{
+		push(dst_head, src_head, stack);
+		i--;
+	}
 }
 
 void	brut_force(t_node **a_head, t_node **b_head, int size, char stack)
