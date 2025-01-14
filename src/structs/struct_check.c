61927c99fddcd53e53fd6d166eb726047961d57f
Alexandre Lacroix <alacroix@bess-f2r4s10.clusters.42paris.fr>
2025-01-14 17:56:48 +0100

    fix stack is sorted

--- src/structs/struct_check.c
+++ src/structs/struct_check.c
@@ -6,7 +6,7 @@
 /*   By: alacroix <alacroix@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 18:15:20 by alacroix          #+#    #+#             */
-/*   Updated: 2025/01/13 16:34:52 by alacroix         ###   ########.fr       */
+/*   Updated: 2025/01/14 17:56:10 by alacroix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ bool	stack_is_sorted(t_node *a_head)
 {
 	t_node	*current;
 
+	if (!a_head)
+		return (false);
 	current = a_head;
 	while (current->next != a_head)
 	{
